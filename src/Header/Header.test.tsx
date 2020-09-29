0edb9d80b18a3f75a154164a27e3d4bbd8ee0bdf
Oscar Mauricio Yepes Morato <oscarmyepes@gmail.com>
2020-09-29 11:57:54 -0500

    Test

--- src/Header/Header.test.tsx
+++ src/Header/Header.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import Header from './Header';
+import { Header } from './Header';
 
 describe('test', () => {
   it('Should render header', () => {
