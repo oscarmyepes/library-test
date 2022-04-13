77ab903993378c49b1507f1187b5e01795967817
Oscar Mauricio Yepes Morato <oscarmyepes@gmail.com>
2022-04-13 16:11:34 -0500

    Test

--- pubsub-subscriber.js
+++ pubsub-subscriber.js
@@ -3,25 +3,13 @@ console.log(
   "FIREBASE_ADMIN_CLIENT_EMAIL",
   process.env.FIREBASE_ADMIN_CLIENT_EMAIL
 );
+
 console.log(
-  "FIREBASE_ADMIN_PRIVATE_KEY",
-  process.env.FIREBASE_ADMIN_PRIVATE_KEY
-);
-console.log(
-  "FIREBASE_ADMIN_PRIVATE_KEY2",
-  process.env.FIREBASE_ADMIN_PRIVATE_KEY2
+  "FIREBASE_ADMIN_CLIENT_EMAIL2",
+  process.env.FIREBASE_ADMIN_CLIENT_EMAIL2
 );
 
-console.log("WEBSITE_ADDRESS", process.env.WEBSITE_ADDRESS);
 console.log(
-  "NEXT_PUBLIC_FIREBASE_PROJECT_ID",
-  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+  "FIREBASE_ADMIN_CLIENT_EMAIL3",
+  process.env.FIREBASE_ADMIN_CLIENT_EMAIL3
 );
-
-const value = process.env.FIREBASE_ADMIN_CLIENT_EMAIL + "in script";
-console.log("XXXXXXXXXXXXXX", value);
-
-console.log(process.env.FIREBASE_ADMIN_CLIENT_EMAIL2);
-console.log(process.env.FIREBASE_ADMIN_CLIENT_EMAIL3);
-console.log(process.env.TEST);
-console.log(process.env);
